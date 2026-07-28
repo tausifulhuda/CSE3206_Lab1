1cd0c6bdba6dfe25c694d73f38c537c21020d573
Ruhul Amin Pappo <papporuhulamin@gmail.com>
2026-07-28 11:49:11 +0600

    added a function which prints the course code and course name

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,11 @@
 def printHelloWorld():
     print("Hello World")
+def firstLab():
+    print("This is the first lab of CSE 3206: Software Engineering Sessional")
+
+
 
 if __name__=="__main__":
     printHelloWorld()
+    firstLab()
+
